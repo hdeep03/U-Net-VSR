df723a4109a89fa8a4f2a2e79ab2d39ea014ea98
hdeep03 <hdeep03@gmail.com>
2024-04-28 22:32:13 -0400

    Speed up dataloader

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -3,8 +3,7 @@ import os
 from torch.utils.data import IterableDataset
 import cv2
 import torch
-import numpy as np
-from collections import deque
+from tqdm import tqdm
 from utils import downsample, bicubic_upsample
 
 class VSRDataset(IterableDataset):
@@ -13,21 +12,32 @@ class VSRDataset(IterableDataset):
         self.videos = [f for f in os.listdir(data_dir) if f.endswith(".webm")]
         self.buffer_size = buffer_size
 
-    def process_buffer(self, buffer) -> Tuple[torch.Tensor, torch.Tensor]:
-        y = torch.tensor(np.array(buffer))
-        y = y.permute(0, 3, 1, 2)
+    def process_image(self, image) -> Tuple[torch.Tensor, torch.Tensor]:
+        y = torch.tensor(image, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)/255.0
         x = bicubic_upsample(downsample(y))
         return x, y
 
     def __iter__(self):
         for video in self.videos:
-            buffer = deque([])
+            buffer = torch.empty((0, 3, 720, 1280))
             vidcap = cv2.VideoCapture(os.path.join(self.data_dir, video))
             success = True
             while success:
                 success, image = vidcap.read()
-                buffer.append(image)
-                if len(buffer) < self.buffer_size:
+                out_x, out_y = self.process_image(image)
+                buffer = torch.cat((buffer, out_x), dim=0)
+                if buffer.shape[0] < self.buffer_size:
                     continue
-                yield self.process_buffer(buffer)
-                buffer.popleft()
+                yield buffer, out_y
+                buffer = buffer[1:]
+
+
+if __name__ == "__main__":
+    dataset = VSRDataset("./data/raw/val")
+    for x, y in tqdm(dataset):
+        pass
+        # first_frame = x[0].permute(1, 2, 0).numpy()
+        # cv2.imshow("First frame", first_frame)
+        # cv2.imshow("Ground truth", y[0].permute(1, 2, 0).numpy())
+        # cv2.waitKey(0)
+        # exit()
--- src/data/utils.py
+++ src/data/utils.py
@@ -2,14 +2,8 @@ import torch
 import torch.nn.functional as F
 
 def downsample(x: torch.Tensor) -> torch.Tensor:
-    new_height = 360
-    new_width = int(new_height * 16 / 9)
-    x_resized = F.interpolate(x, size=(new_height, new_width), mode='bilinear', align_corners=False)
-    return x_resized
+    return F.interpolate(x, size=(360, 640), mode='bilinear', align_corners=False)
 
 
 def bicubic_upsample(x: torch.Tensor) -> torch.Tensor:
-    new_height = 720
-    new_width = int(new_height * 16 / 9)
-    x_resized = F.interpolate(x, size=(new_height, new_width), mode='bicubic', align_corners=False)
-    return x_resized
+    return F.interpolate(x, size=(720, 1280), mode='bicubic', align_corners=False)
