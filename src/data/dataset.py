526d36dbb01f76d5641ec28c7b346b02cbe5cb57
hdeep03 <hdeep03@gmail.com>
2024-04-28 22:56:03 -0400

    Add patch option to dataloader

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -7,15 +7,22 @@ from tqdm import tqdm
 from utils import downsample, bicubic_upsample
 
 class VSRDataset(IterableDataset):
-    def __init__(self, data_dir, buffer_size=10):
+    def __init__(self, data_dir, buffer_size=10, patch=False):
         self.data_dir = data_dir
         self.videos = [f for f in os.listdir(data_dir) if f.endswith(".webm")]
         self.buffer_size = buffer_size
+        self.patch = patch
 
     def process_image(self, image) -> Tuple[torch.Tensor, torch.Tensor]:
         y = torch.tensor(image, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)/255.0
         x = bicubic_upsample(downsample(y))
         return x, y
+
+    def extract_patches(self, x, y):
+        ex = lambda im, px, py: im[:, :, 80*(py):80*(py+1), 80*(px):80*(px+1)]
+        for i in range(16):
+            for j in range(9):
+                yield ex(x, j, i), ex(y, j, i)
 
     def __iter__(self):
         for video in self.videos:
@@ -28,13 +35,18 @@ class VSRDataset(IterableDataset):
                 buffer = torch.cat((buffer, out_x), dim=0)
                 if buffer.shape[0] < self.buffer_size:
                     continue
-                yield buffer, out_y
+
+                if not self.patch:
+                    yield buffer, out_y
+                else:
+                    yield from self.extract_patches(buffer, out_y)
                 buffer = buffer[1:]
 
 
 if __name__ == "__main__":
-    dataset = VSRDataset("./data/raw/val")
-    for x, y in tqdm(dataset):
+    dataset = VSRDataset("./data/raw/val", patch=True)
+    dataloader = torch.utils.data.DataLoader(dataset)
+    for x, y in tqdm(dataloader):
         pass
         # first_frame = x[0].permute(1, 2, 0).numpy()
         # cv2.imshow("First frame", first_frame)
