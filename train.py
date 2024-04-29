f0df2e68f2058d0724016eb2ddd540561920214c
Alex Hu <superalexanderhu@gmail.com>
2024-04-29 12:40:18 -0400

    add wandb

--- train.py
+++ train.py
@@ -5,7 +5,8 @@ import argparse
 import os
 import logging
 from tqdm import tqdm
-
+import time
+import wandb
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,12 +20,16 @@ if __name__ == "__main__":
     parser.add_argument("--patch", action="store_true")
     parser.add_argument("--lr", type=float, default=0.0001)
     parser.add_argument("--log_every", type=int, default=100)
+    parser.add_argument("--save_every", type=int, default=1000)
 
     args = parser.parse_args()
-    logging.basicConfig(level = logging.INFO, filename = os.path.join(args.run_dir ,"train.log"), filemode = "w", format = '%(name)s - %(levelname)s - %(message)s')
+    os.makedirs(args.run_dir, exist_ok=True)
+    logging.basicConfig(level = logging.INFO, filename = os.path.join(args.run_dir ,"train.log"), filemode = "a", format = '%(name)s - %(levelname)s - %(message)s')
     logging.info(f"args: {args}")
 
-    os.makedirs(args.run_dir, exist_ok=True)
+    wandb.init(project="VSR", name=args.run_dir, config=args)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     if args.baseline:
         model = UNet(3)
@@ -35,6 +40,8 @@ if __name__ == "__main__":
         dataset = VSRDataset(args.data_path, buffer_size=args.num_frames, patch=args.patch)
         valset = VSRDataset(args.val_path, buffer_size=args.num_frames, patch=args.patch)
     
+    model = model.to(device)
+
     ckpts = sorted([int(f.split("_")[1].split(".")[0]) for f in os.listdir(args.run_dir) if f.endswith(".pth")])
     if ckpts:
         model.load_state_dict(torch.load(f"{args.run_dir}/model_{ckpts[-1]}.pth"))
@@ -44,23 +51,21 @@ if __name__ == "__main__":
     valloader = torch.utils.data.DataLoader(valset, batch_size=args.batch_size)
 
     logging.info(f"dataset size: {len(dataset)}")
-    
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
     optimizer = torch.optim.AdamW(model.parameters(), lr = args.lr)
 
-
-
+    j = 0
     for i in range(args.epochs):
         logging.info(f"epoch: {i}")
-        j = 0
-        model.train()
+
+        time1 = time.time()
         for x, y in tqdm(dataloader):        
+            model.train()
             # x : [batch_size, nframes, 3, 64, 64]
             # y: [batch_size, 3, 64, 64]
             x = x.to(device)
             y = y.to(device)
             x = x.reshape(x.shape[0], -1, x.shape[-2], x.shape[-1])
+            y = y.reshape(y.shape[0], -1, y.shape[-2], y.shape[-1])
             optimizer.zero_grad()
             ypred = model(x)
             loss = torch.nn.functional.mse_loss(ypred, y)
@@ -69,19 +74,30 @@ if __name__ == "__main__":
     
             j += 1
             if j % args.log_every == 0:
-                logging.info(f"loss: {loss}")
+                logging.info(f"loss: {loss}, sample/s = {args.log_every * x.shape[0] / (time.time() - time1)}")
+                wandb.log({"train_loss": loss, "samples_per_second": args.log_every * x.shape[0] / (time.time() - time1)})
+                time1 = time.time()
+            if j % args.save_every == 0:
+                torch.save(model.state_dict(), os.path.join(args.run_dir, f"model_{j}.pth"))
+                logging.info(f"model saved at {args.run_dir}/model_{j}.pth")
         
-        torch.save(model.state_dict(), os.path.join(args.run_dir, f"model_{i}.pth"))
-        logging.info(f"model saved at {args.run_dir}/model_{i}.pth")
-        model.eval()
-        with torch.inference_mode():
-            total_loss = 0.0
-            for x, y in tqdm(valloader):
-                x = x.to(device)
-                y = y.to(device)
-                x = x.reshape(x.shape[0], -1, x.shape[-2], x.shape[-1])
-                ypred = model(x)
-                total_loss += torch.nn.functional.mse_loss(ypred, y).item()
-            logging.info(f"avg val_loss: {total_loss / len(valset)}")
+                model.eval()
+                num_val_samples = 100
+                with torch.inference_mode():
+                    total_loss = 0.0
+                    i = 0
+                    for x, y in tqdm(valloader):
+                        x = x.to(device)
+                        y = y.to(device)
+                        x = x.reshape(x.shape[0], -1, x.shape[-2], x.shape[-1])
+                        y = y.reshape(y.shape[0], -1, y.shape[-2], y.shape[-1])
+                        ypred = model(x)
+                        total_loss += torch.nn.functional.mse_loss(ypred, y).item()
+                        i += 1
+                        if i == num_val_samples:
+                            break
+                    logging.info(f"avg val_loss: {total_loss / num_val_samples}")
+                    wandb.log({"val_loss": total_loss / num_val_samples})
     
 
+    wandb.finish()
