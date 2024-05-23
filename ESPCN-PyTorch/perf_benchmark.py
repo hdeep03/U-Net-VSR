de4cc5171b07c7e34a64c83035b957070673470e
hdeep03 <hdeep03@gmail.com>
2024-05-23 10:18:19 -0400

    Test perf w/ coreml

--- ESPCN-PyTorch/perf_benchmark.py
+++ ESPCN-PyTorch/perf_benchmark.py
@@ -1,4 +1,5 @@
 import torch
+import time
 import numpy as np
 from model import espcn_x4
 from tqdm import trange
@@ -6,24 +7,25 @@ from tqdm import trange
 mps_device = torch.device("mps")
 
 model = espcn_x4(in_channels=1, out_channels=1, channels=64).to(mps_device)
+# compiled_model = model.eval()
 compiled_model = torch.jit.optimize_for_inference(torch.jit.script(model.eval()))
 
 # Warm-up phase
-for _ in range(10):
+for _ in trange(10):
     warmup_data = torch.randn(1, 1, 180, 320, device=mps_device)
-    compiled_model(warmup_data)
+    out = compiled_model(warmup_data)
+    print(out.shape)
 
 total_time = []
-for x in trange(24):
+for x in trange(100):
     frame_data = torch.randn(1, 1, 180, 320, device=mps_device)
-    start = torch.mps.Event(enable_timing=True)
-    end = torch.mps.Event(enable_timing=True)
+    # start = torch.mps.Event(enable_timing=True)
+    # end = torch.mps.Event(enable_timing=True)
     
-    start.record()
+    start = time.perf_counter()
     compiled_model(frame_data)
-    end.record()
-    
-    # end.synchronize()  # Ensure the end event has completed
-    total_time.append(start.elapsed_time(end))
+    end = time.perf_counter()
+    # torch.mps.synchronize()
+    total_time.append((end - start)*1000)
 
 print("AVG: {}; STD: {}".format(np.mean(total_time), np.std(total_time)))
--- perf_benchmark.py
+++ perf_benchmark.py
@@ -1,22 +1,32 @@
 import torch
 import numpy as np
-from src.model.unet_model import RUNetSmall
+# from src.model.unet_model import RUNetSmall, RUNet, UNet
 from tqdm import trange
+import coremltools as ct
+import coremltools.optimize.coreml as cto
+import time
 
-mps_device = torch.device("mps")
+# mps_device = torch.device("mps")
 
-model = RUNetSmall(30, use_tanh=True).to(mps_device)
-compiled_model = torch.jit.optimize_for_inference(torch.jit.script(model.eval()))
+
+
+model = ct.models.MLModel('runet_small.mlpackage')
+print(model.get_spec().description.input)
+
+op_config = cto.OpPalettizerConfig(mode="kmeans", nbits=4)
+
+# define optimization config by applying the op config globally to all ops 
+config = cto.OptimizationConfig(global_config=op_config)
+
+# palettize weights
+compressed_mlmodel = cto.palettize_weights(model, config)
 
 total_time = []
-for x in trange(24):
-    frame_data = torch.randn(1, 30, 720, 1280, device=mps_device)
-    start = torch.mps.Event(enable_timing=True)
-    end = torch.mps.Event(enable_timing=True)
-    start.record()
-    compiled_model(frame_data)
-    end.record()
-    torch.mps.synchronize()
-    total_time.append(start.elapsed_time(end))
+for x in trange(200):
+    frame_data = np.random.rand(144, 30, 80, 80)
+    start = time.perf_counter()
+    output_dict = compressed_mlmodel.predict({'x': frame_data})
+    # print(output_dict['var_331'].shape)
+    total_time.append(time.perf_counter() - start)
 
 print("AVG: {}; STD: {}".format(np.mean(total_time), np.std(total_time)))
