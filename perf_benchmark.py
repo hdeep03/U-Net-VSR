bc17b395a51c3ebdf188aee495c2a6a233e3d16c
hdeep03 <hdeep03@gmail.com>
2024-05-23 15:47:38 -0400

    Add palettization for coreml

--- perf_benchmark.py
+++ perf_benchmark.py
@@ -13,7 +13,7 @@ import time
 model = ct.models.MLModel('runet_small.mlpackage')
 print(model.get_spec().description.input)
 
-op_config = cto.OpPalettizerConfig(mode="kmeans", nbits=4)
+op_config = cto.OpPalettizerConfig(mode="uniform")
 
 # define optimization config by applying the op config globally to all ops 
 config = cto.OptimizationConfig(global_config=op_config)
@@ -22,11 +22,16 @@ config = cto.OptimizationConfig(global_config=op_config)
 compressed_mlmodel = cto.palettize_weights(model, config)
 
 total_time = []
+error = []
 for x in trange(200):
     frame_data = np.random.rand(144, 30, 80, 80)
     start = time.perf_counter()
-    output_dict = compressed_mlmodel.predict({'x': frame_data})
+    compressed_out = compressed_mlmodel.predict({'x': frame_data})
+    normal_out = model.predict({'x': frame_data})
+    loss = np.sum((compressed_out['var_331'] - normal_out['var_331'])**2)
+    error.append(loss)
     # print(output_dict['var_331'].shape)
     total_time.append(time.perf_counter() - start)
 
+print("AVG LOSS: {}; STD LOSS: {}".format(np.mean(error), np.std(error)))
 print("AVG: {}; STD: {}".format(np.mean(total_time), np.std(total_time)))
